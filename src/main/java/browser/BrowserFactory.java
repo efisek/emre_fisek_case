81f6448a578a8515fb8cf150e151e830cd5f7a80
efisek <fisekemre@gmail.com>
2025-08-12 00:22:16 +0300

    "infrastructure including broswer, conditions, config created"

---
+++ src/main/java/browser/BrowserFactory.java
@@ -0,0 +1,20 @@
+package browser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public final class BrowserFactory {
+    private static final Map<String, Supplier<BrowserSetup>> browsers = new HashMap<>();
+
+    static {
+        browsers.put("chrome", ChromeBrowserSetup::new);
+        //it can be implemented for other browser
+        //browsers.put("firefox", FirefoxBrowserSetup::new);
+
+    }
+
+    public static BrowserSetup getBrowserSetup(String browserName) {
+        return browsers.getOrDefault(browserName.toLowerCase(), ChromeBrowserSetup::new).get();
+    }
+}
